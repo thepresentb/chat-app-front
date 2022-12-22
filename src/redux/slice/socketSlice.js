c46049078c5a04420bf778db225c7f284af5c757
MacbookPro <macbookpro@MacbookPros-MacBook-Pro.local>
2022-12-22 12:00:20 +0700

    new

--- src/redux/slice/socketSlice.js
+++ src/redux/slice/socketSlice.js
@@ -10,7 +10,7 @@ const socketSlice = createSlice({
   initialState,
   reducers: {
     initSocket: (state) => {
-      state.socket = io("https://localhost:3000", {
+      state.socket = io("https://chat-app-liard-zeta.vercel.app:3000", {
         withCredentials: true,
         extraHeaders: {
           "my-custom-header": "present",
